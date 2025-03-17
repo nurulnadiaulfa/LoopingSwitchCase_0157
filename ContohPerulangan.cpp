3edae2514e8ac75765f29fc2a27106627d4a2a82
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-03-17 15:34:47 +0700

    menambhakan perulangan pencacah naik

--- ContohPerulangan.cpp
+++ ContohPerulangan.cpp
@@ -8,4 +8,14 @@ int main()
     int n;
     int perulanganWhile = 0;
     int perulanganDo = 0;
+    
+    //perulangan dengan for
+
+    cout << "Perulangan for pencacah naik" <<endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n = " << n << " Selamat Datang" <<endl;
+    }
+    cout << "Nilai n terakhir = " << n <<endl;
+    cout <<endl;
 }
