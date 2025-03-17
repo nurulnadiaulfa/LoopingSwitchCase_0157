665f0b125c4e26d7f2f1a2a044eb6d39d7eabec3
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-03-17 15:49:38 +0700

    menambahkan perulangan while

--- ContohPerulangan.cpp
+++ ContohPerulangan.cpp
@@ -11,20 +11,37 @@ int main()
     
     //perulangan dengan for
 
-    cout << "Perulangan for pencacah naik" <<endl;
+    cout << "Perulangan for pencacah naik" << endl;
     for (n = 0; n < 5; n++)
     {
-        cout << "Nilai n = " << n << " Selamat Datang" <<endl;
+        cout << "Nilai n = " << n << " Selamat Datang" << endl;
     }
-    cout << "Nilai n terakhir = " << n <<endl;
+    cout << "Nilai n terakhir = " << n << endl;
     cout << endl;
 
-    cout << "Perulangan for pencacah turun" <<endl;
+    cout << "Perulangan for pencacah turun" << endl;
     // Perulangan dengan for
     for (n = 5; n > 0; n--)
     {
-        cout << "Nilai n = " << n << " Selamat datang" <<endl;
+        cout << "Nilai n = " << n << " Selamat datang" << endl;
     }
-    cout << "Nilai n terakhir = " << n <<endl;
+    cout << "Nilai n terakhir = " << n;
     cout << endl;
+
+    cout << "Perulangan while" << endl;
+
+    srand(time(0));
+
+    n = rand() % 10;
+
+    cout << "Nilai awal n = " << n << endl;
+
+    while (n < 7)
+    {
+        cout << "Nilai awal n = " << n << endl;
+        n = rand() % 10;
+        perulanganWhile++;
+    }
+    cout << "Jumlah perulangan = " << perulanganWhile << endl;
+    cout << "Nilai n terakhir = " << n << endl;
 }
